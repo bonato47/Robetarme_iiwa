3236ce5527038796d5176203361e26f3b6c7c0e6
bonato47 <61695400+bonato47@users.noreply.github.com>
2023-08-07 16:59:27 +0200

    Update Ds_follower.cpp

--- src/send_pos/src/Ds_follower.cpp
+++ src/send_pos/src/Ds_follower.cpp
@@ -213,7 +213,7 @@ int main(int argc, char **argv)
         vector<double> NextQuatPosCart = Integral_func(actualState.posCartActual, speed_eigen, delta_t);
         
         //get inverse kinematic 
-        nextState.getIK(NextQuatPosCart);
+        //nextState.getIK(NextQuatPosCart);
 
         //-----------------------------------------------------------------------
         //send next joint 
