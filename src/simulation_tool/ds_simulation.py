1bf9f6b64c4953356c1a47b44a6abff769a7ed38
Louis Munier <louis.munier@epfl.ch>
2023-09-08 15:30:34 +0200

    [ROS] Integrate ROS subscribers / publishers and reorganize a bit the code.

--- src/simulation_tool/ds_simulation.py
+++ src/simulation_tool/ds_simulation.py
@@ -2,7 +2,6 @@ import os
 import xml.etree.ElementTree as ET
 import rospy
 
-from math import radians
 from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import JointState
 
@@ -11,6 +10,8 @@ from typing import List, Tuple
 from coppeliasim_zmqremoteapi_client import *
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
+
+VERBOSE = False
 PATH_SCENE = current_dir + "/ds_trial.ttt"
 FREQUENCY = 100
 
@@ -18,7 +19,7 @@ URDF_FILE = current_dir + "/urdf/iiwa7.urdf"
 
 @dataclass()
 class Robot():
-    """Robot dataclass."""
+    """ Robot dataclass. """
     name: str = ""
     nb_dofs: int = 0
     max_limits: Tuple[int, int] = (0.0, 0.0) # [rad]
@@ -37,23 +38,24 @@ class Robot():
     lst_max_torque: List[int] = field(default_factory=list) # [rad/s^3]
 
     def print(self):
-        """Print the robot informations."""
-        print(f"Robot informations:")
+        """ Print the robot informations. """
+        print("Robot informations:")
         for key, value in asdict(self).items():
             print(f"\t{key}: {value}")
 
-client = RemoteAPIClient()
-sim = client.getObject("sim")
-client.setStepping(True)
 
 def main():
-    """Main function."""
+    """ Main function. """
     kuka_iiwa14 = read_urdf(URDF_FILE)
-    sim.loadScene(PATH_SCENE)
+    if VERBOSE:
+        kuka_iiwa14.print()
 
-    link_joints(kuka_iiwa14)
-    kuka_iiwa14.print()
     setup_rostopics()
+
+    client = RemoteAPIClient()
+    sim = client.getObject("sim")
+    client.setStepping(True)
+    setup_simulation(sim, kuka_iiwa14)
 
     sim.startSimulation()
     while sim.getSimulationState() != sim.simulation_stopped:
@@ -99,22 +101,53 @@ def read_urdf(str_urdf: str) -> Robot:
 
     return new_robot
 
-def link_joints(robot: Robot):
+
+def link_joints(robot: Robot, sim: any):
     """ Link the joint to the robot.
 
     args:
         robot (Robot): Robot dataclass.
+        sim (any): CoppeliaSim simulated client.
     """
     for i in range(robot.nb_dofs):
         robot.lst_joints.append(sim.getObject("./joint", {'index': i}))
 
+
 def setup_rostopics():
-    """Setup all the different ROS requirements."""
+    """ Setup all the different ROS requirements. """
     rospy.init_node("ds_simulation", anonymous=True)
     ros_rate = rospy.Rate(FREQUENCY)
     rospy.sleep(2)
 
+    # Subscriber
+    rospy.Subscriber("/iiwa/TorqueController/command", Float64MultiArray, cbk_torque_command)
+
+    # Publisher
+    rospy.Publisher("/iiwa/JointStates", Float64MultiArray, queue_size=10)
+
+
     return ros_rate
+
+
+def setup_simulation(sim: any, robot: Robot):
+    """ Setup the simulation using the parameters passed in input.
+
+    args:
+        sim (any): CoppeliaSim simulated client.
+        robot (Robot): Robot dataclass.
+    """
+    sim.loadScene(PATH_SCENE)
+
+    link_joints(robot, sim)
+
+
+def cbk_torque_command(msg_torques: Float64MultiArray):
+    """ Callback function to retrieve the torques from the controller.
+
+    args:
+        msg_torques (Float64MultiArray): Torques from the controller.
+    """
+    pass
 
 
 if __name__ == "__main__":
